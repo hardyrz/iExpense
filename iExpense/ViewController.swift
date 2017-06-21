574c09e9f3c3c0922dcd11ac1b022bc6c2685c6f
Brian Hardy <hardyr.93@gmail.com>
2017-06-20 22:00:19 -0300

    Initial commit

---
+++ iExpense/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iExpense
+//
+//  Created by SP 23 on 20/6/17.
+//  Copyright © 2017 SP 23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
