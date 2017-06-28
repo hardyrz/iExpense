2f9f6650739b88d81bd56a7e0c969da2c6c123a8
Brian Hardy <hardyr.93@gmail.com>
2017-06-27 22:09:23 -0300

    Category table hardcoded data

--- iExpense/CategoriesViewController.swift
+++ iExpense/CategoriesViewController.swift
@@ -1,6 +1,9 @@
 import UIKit
 
-class CategoriesViewController: UIViewController {
+class CategoriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var categoriesTable : [CategoryDTO]?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -12,5 +15,20 @@ class CategoriesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //if let table = self.expensesTable {
+        //    return table.count
+        //}
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath) as! CategoryCell
+        
+        cell.DescriptionLabel.text = "Food" //categoriesTable[indexPath.row].description
+        cell.CategoryImage.image = UIImage(named: "food_icon") //categoriesTable[indexPath.row].image
+        
+        return cell
+    }
     
 }
